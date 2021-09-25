c5dd235f1af82d4e109b0b0c41f8de100e6ccee6
LNCE21 <lautaronicolascc@gmail.com>
2021-09-25 20:06:36 -0300

    Added new excercises of Operators: Mathematical, Assignment, Comparison

--- assignments.js
+++ assignments.js
@@ -14,9 +14,7 @@ console.log(country, continent, population);
 
 //LECTURE: Data Types
 
-/*1. Declare a variable called 'isIsland' and set its value according to your
-country. The variable should hold a Boolean value. Also declare a variable
-'language', but don't assign it any value yet
+/*1. Declare a variable called 'isIsland' and set its value according to your country. The variable should hold a Boolean value. Also declare a variable 'language', but don't assign it any value yet
 let isIsland = false;
 let language;
 */
@@ -32,7 +30,7 @@ console.log(typeof language);
 //LECTURE: let, const and var
 
 /*1. Set the value of 'language' to the language spoken where you live (some countries have multiple languages, but just choose one)*/
-language = "Castellano"; 
+language = "castellano"; 
 
 
 /*2. Think about which variables should be const variables (which values will never change, and which might change?). Then, change these variables to const.*/
@@ -40,3 +38,30 @@ const country = "Argentina";
 const continent = "America";
 const isIsland = false; //My real country is Argentina.
 /*3. Try to change one of the changed variables now, and observe what happens*/
+
+
+//LECTURE: Basic Operators
+/*1. If your country split in half, and each half would contain half the population, then how many people would live in each half?
+
+let half = population / 2;
+console.log(country, " would have ", half, " million people.");
+*/
+
+/*2. Increase the population of your country by 1 and log the result to the console
+population ++;
+console.log(country, " has ", population, " million people.");
+*/
+
+/*3. Finland has a population of 6 million. Does your country have more people than Finland?
+let finland = 6000000;
+console.log(population > finland);
+*/
+
+/*4. The average population of a country is 33 million people. Does your country have less people than the average country?
+const averagePopulation = 33000000;
+console.log(population > averagePopulation);
+*/
+
+/*5. Based on the variables you created, create a new variable 'description' which contains a string with this format: 'Portugal is in Europe, and its 11 million people speak portuguese'
+let description = country + " is in " + continent + ", " + "and its " + population + " million people speak " + language + ".";
+console.log(description);*/
--- script.js
+++ script.js
@@ -1,8 +1,40 @@
 /*
-
+Variables
 let js = "amazing";
 console.log(40 + 8 + 23 - 10);
 let firstName = "Jonas";
 console.log(firstName);
-
 */
+
+/* 
+Operators
+
+// Math operators
+let now = 2021;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+//2 ** 3 --> Means 2 to the power of 3 --> 2*2*2
+
+// Plus operator to concatenate strings
+const firstName = "Jonas ";
+const lastName = "Schmedtmann";
+console.log(firstName + lastName);
+
+// Assignment operators
+let x = 10 + 5;
+console.log(x);
+x += 10; // Means x = x + 10
+console.log(x); 
+x *= 4;  // Means x = x * 4
+x ++ //Means x + 1
+
+// Comparison operators
+// <  > =  >=  <= 
+console.log(ageJonas > ageSarah); 
+console.log(ageSarah >= 18);
+*/
+
+/**/
+
