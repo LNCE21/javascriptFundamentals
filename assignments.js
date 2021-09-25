e17181dd9719cb9bede96d565a6c6eae614fe9da
LNCE21 <lautaronicolascc@gmail.com>
2021-09-25 17:09:31 -0300

    Ended of practicing data types and let const var

--- assignments.js
+++ assignments.js
@@ -2,20 +2,41 @@
 
 /*1. Declare variables called 'country', 'continent' and 'population' and
 assign their values according to your own country (population in millions)*/
+/*
 let country = "Argentina";
 let continent = "America";
+*/
 let population = 44940000;
 
-/*2. Log their values to the console*/
+/*2. Log their values to the console
 console.log(country, continent, population);
+*/
 
 //LECTURE: Data Types
+
 /*1. Declare a variable called 'isIsland' and set its value according to your
 country. The variable should hold a Boolean value. Also declare a variable
-'language', but don't assign it any value yet*/
-
-
-
+'language', but don't assign it any value yet
+let isIsland = false;
+let language;
+*/
 
 /*2. Log the types of 'isIsland', 'population', 'country' and 'language'
-to the console*/
+to the console
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+*/
+
+//LECTURE: let, const and var
+
+/*1. Set the value of 'language' to the language spoken where you live (some countries have multiple languages, but just choose one)*/
+language = "Castellano"; 
+
+
+/*2. Think about which variables should be const variables (which values will never change, and which might change?). Then, change these variables to const.*/
+const country = "Argentina";
+const continent = "America";
+const isIsland = false; //My real country is Argentina.
+/*3. Try to change one of the changed variables now, and observe what happens*/
