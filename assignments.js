e3b7673fd9383e22c2883568597c298f5293209b
LNCE21 <lautaronicolascc@gmail.com>
2021-10-05 00:03:27 -0300

    Added notes on The Switch Statement lecture (alternative to if-else), an assignment and the solution

--- assignments.js
+++ assignments.js
@@ -227,3 +227,36 @@ I've already did something similar
 /* 5. Probably your country does not meet all the criteria. So go back and temporarily change some variables in order to make the condition true (unless you live in Canada :D)
 Done
 */
+
+
+
+//LECTURE: The Switch Statement
+/*1. Use a switch statement to log the following string for the given 'language': 
+chinese or mandarin: 'MOST number of native speakers!' spanish: '2nd place in number of native speakers' 
+english: '3rd place' 
+hindi: 'Number 4' 
+arabic: '5th most spoken language' 
+for all other simply log 'Great language too :D'
+
+const language = "spanish";
+switch (language) {
+    case "chinese":
+    case "mandarin":
+        console.log("MOST number of native speakers!");
+        break;
+    case "spanish":
+        console.log("2nd place in number of native speakers");
+        break;
+    case "english":
+        console.log("3rd place");
+        break;
+    case "hindi":
+        console.log("Number 4");
+        break;
+    case "arabic":
+        console.log("5th most spoken language");
+        break;
+    default: 
+    console.log("Great language too :D");
+}
+*/
--- script.js
+++ script.js
@@ -254,3 +254,34 @@ if(hasDriverLicense && hasGoodVision && !isTired) {
     console.log("Someone else should drive...");
 }
 */
+
+
+
+/*SWITCH STATEMENTS: alternative way to write an if-else statement when all we want to do is to compare one value to multiple different options
+
+//In the next example we are going to map one activity to each day:
+const day = "monday";
+//This switch statement will compare "day" to "monday" in a strict equality way 
+switch(day) {
+    case "monday":
+        console.log("Plan course structure");
+        console.log("Go to coding meetup") ;
+        break;
+    case "tuesday":
+        console.log("Prepare theory videos");
+        break;
+    case "wednesday":
+    case "thursday":
+        console.log("Write code examples");
+        break;
+    case "friday":
+        console.log("Record videos");
+        break;
+    case "saturday":
+    case "sunday":
+        console.log("Enjoy the weekend")
+        break;
+    default:
+        console.log("Not a valid day!");
+}
+*/
