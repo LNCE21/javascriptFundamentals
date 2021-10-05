837d942d169d44f5354ae6bab42285166934ca14
LNCE21 <lautaronicolascc@gmail.com>
2021-10-05 13:15:33 -0300

    Added a newfeature to the last challenge

--- challenges.js
+++ challenges.js
@@ -166,10 +166,10 @@ GOOD LUCK*/
 Steven wants to build a very simple tip calculator for whenever he goes eating in a restaurant. In his country, it's usual to tip 15% if the bill value is between 50 and 300. If the value is different, the tip is 20%. Your tasks:*/
 
 /*1. Calculate the tip, depending on the bill value. Create a variable called 'tip' for this. It's not allowed to use an if/else statement (If it's easier for you, you can start with an if/else statement, and then try to convert it to a ternary operator!)
-const billValue = 50;
-billValue >= 50 && billValue <= 300 ? console.log(`According to the bill value $${billValue}, the tip you have to leave is $ ${billValue * 0.15}.`) : console.log(`According to the bill value $${billValue}, the tip you have to leave is $ ${billValue * 0.2}.`);
-
+const billValue = 275;
+billValue >= 50 && billValue <= 300 ? console.log(`According to the bill value $${billValue}, the tip you have to leave is $ ${billValue * 0.15}. The total value is $${billValue + billValue * 0.15}`) : console.log(`According to the bill value $${billValue}, the tip you have to leave is $ ${billValue * 0.2}. The total value is $${billValue + billValue * 0.2}`);
 */
+
 
 /*2. Print a string to the console containing the bill value, the tip, and the final value
 (bill + tip). Example: “The bill was 275, the tip was 41.25, and the total value
