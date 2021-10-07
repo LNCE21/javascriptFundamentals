295ef981e425a36f3f0476b7d7dbe74af7037b7e
LNCE21 <lautaronicolascc@gmail.com>
2021-10-07 16:09:24 -0300

    Added new lecture about functions

--- fundamentalsPart2.js
+++ fundamentalsPart2.js
@@ -16,4 +16,34 @@ if (hasDriversLicense) console.log("I can drive :D");
 
 
 
-/*FUNCTIONS*/
+/*FUNCTIONS
+//Example of logger function, a function that logs into the console.
+
+//Syntax: function keyword, name of function, ();, next follows the curly braces, all the code inside them are part of the "function body" and its the code that will be executed every time we call the function.
+function logger() {
+    console.log("My name is Lautaro");
+}
+
+//Invoking/running/calling the function, we simply write the name with ();
+logger();
+*/
+
+//Example of function with parameters, return
+function fruitProcessor(apples, oranges) {
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+} 
+
+
+//Invoke, call, run the function fruitProcessor by adding arguments (the values of the parameters)
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+//Invoke, call, run the function fruitProcessor by adding a different arguments
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+//Another example of a function is the Number function, is a built-in function, we pass a string as s parameter and returns a number:
+Number("23");
+
+//Another example of a function is the console.log function, it is another built-in function were we pass
