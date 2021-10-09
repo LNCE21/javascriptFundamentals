c831d7317d348bccf4569a449d0bedabf8047e68
LNCE21 <lautaronicolascc@gmail.com>
2021-10-09 16:25:04 -0300

    Added solution to challenge 1 of the functions

--- challenges.js
+++ challenges.js
@@ -202,22 +202,19 @@ DONE
 /*JAVASCRIPT FUNDAMENTALS - PART 2*/
 
 /*Coding Challenge #1
-Back to the two gymnastics teams, the Dolphins and the Koalas! There is a new
-gymnastics discipline, which works differently.
-Each team competes 3 times, and then the average of the 3 scores is calculated (so
-one average score per team).
-A team only wins if it has at least double the average score of the other team.
+Back to the two gymnastics teams, the Dolphins and the Koalas! There is a new gymnastics discipline, which works differently.
+Each team competes 3 times, and then the average of the 3 scores is calculated (so one average score per team). A team only wins if it has at least double the average score of the other team.
 Otherwise, no team wins!
+
 Your tasks:
 1. Create an arrow function 'calcAverage' to calculate the average of 3 scores
 2. Use the function to calculate the average for both teams
 3. Create a function 'checkWinner' that takes the average score of each team
 as parameters ('avgDolhins' and 'avgKoalas'), and then logs the winner
-to the console, together with the victory points, according to the rule above.
-Example: "Koalas win (30 vs. 13)"
-4. Use the 'checkWinner' function to determine the winner for both Data 1 and
-Data 2
+to the console, together with the victory points, according to the rule above. Example: "Koalas win (30 vs. 13)"
+4. Use the 'checkWinner' function to determine the winner for both Data 1 and Data 2
 5. Ignore draws this time
+
 Test data:
 Data 1: Dolphins score 44, 23 and 71. Koalas score 65, 54 and 49
 Data 2: Dolphins score 85, 54 and 41. Koalas score 23, 34 and 27
@@ -225,4 +222,36 @@ Hints:
 To calculate average of 3 values, add them all together and divide by 3
 To check if number A is at least double number B, check for A >= 2 * B.
 Apply this to the team's average scores 
-GOOD LUCK*/
+GOOD LUCK
+
+//1.Arrow function to calculate the average of 3 scores
+const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
+//Calling the function, test...
+console.log(calcAverage(44,23,71));
+
+//2.Use de calcAverage function to calculate the average for both teams
+//Test data 1
+let scoreDolphin = calcAverage(44,23,71);
+let scoreKoala = calcAverage(65,54,49);
+//Calling the function, test...
+console.log(scoreDolphin, scoreKoala);
+
+//3.Function checkWinner with the following parameters: avgDolphins, avgKoalas. Also logs "The winner of the match is ...with a total of ... points" if it matches the double points rule.
+const checkWinner = function(avgDolphins, avgKoalas) {
+    if(avgKoalas * 2 <= avgDolphins) {
+        console.log(`The winners of the match are The Dolphins with a total average of ${avgDolphins} points vs ${avgKoalas} points.`);
+    } else if(avgDolphins * 2 <= avgKoalas) {
+        console.log(`The winners of the match are The Koalas with a total average of ${avgKoalas} points vs ${avgDolphins} points .`);
+    } else {
+        console.log(`It's a draw!`);
+    }
+}
+//Calling the function, test...
+checkWinner(scoreDolphin, scoreKoala);
+
+//4.Function to determine the winner for both Data 1 and Data 2
+//Test data 2
+scoreDolphin = calcAverage(85,54,41);
+scoreKoala = calcAverage(23,34,27);
+checkWinner(scoreDolphin, scoreKoala);
+*/
