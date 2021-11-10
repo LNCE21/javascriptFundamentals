4b54435454ba3c23b1ea9a05c9eef1245885ce0c
LNCE21 <lautaronicolascc@gmail.com>
2021-11-10 11:58:57 -0300

    Added description of scoping lecture

--- howJavascriptWorks.js
+++ howJavascriptWorks.js
@@ -7,13 +7,16 @@ High-level overview of JavaScript, The JavaScript Engine and Runtime, Execution 
 -----------------------------------------------------------
 
 
-// Scoping in Practice
 
+
+/////////////////////////////////////////////////////////////////////////
+// Scoping in Practice
 function calcAge(birthYear) {
-    const age = 2037 - birthYear;
+    const age = 2022 - birthYear;
 
     function printAge() {
-        let output = `${firstName}, you are ${age}, born in ${birthYear}`;
+        //The engine tries to find the "age" variable in the current scope, fails and keeps searching on the parent scope where finally finds it.
+        let output = `Hi ${firstName}!, you are ${age} years old, born in ${birthYear}, right?`;
         console.log(output);
 
         if (birthYear >= 1981 && birthYear <= 1996) {
@@ -21,34 +24,41 @@ function calcAge(birthYear) {
             // Creating NEW variable with same name as outer scope's variable
             const firstName = 'Steven';
 
-            // Reasssigning outer scope's variable
+            // Reasssigning/Redefining outer scope's variable
             output = 'NEW OUTPUT!';
 
             const str = `Oh, and you're a millenial, ${firstName}`;
             console.log(str);
 
+            //Example of functions as block scope starting from ES6 on strict mode
             function add(a, b) {
                 return a + b;
             }
         }
-        // console.log(str);
+        // We can't access the str variable because of the scope chain: console.log(str);
+
+        // We can access millenial because is defined as var, and so, as function scope:
         console.log(millenial);
-        // console.log(add(2, 3));
+
+        // We can't access the add function because of the scope chain, this js file is on strict mode and functions are block scope since ES6 when they are in strict mode: console.log(add(2, 3));
+
+        //We can access output variable because it was declared with function scope, and appears the new output because is called after being reassigned:
         console.log(output);
     }
+    //Calling function
     printAge();
 
     return age;
 }
+// The variable "firstName" is defined after the "calcAge" function but that's not a problem because is only executed once it's actually called, and it has been called after the "firstN ame" declaration
+const firstName = 'Lautaro';
+calcAge(1993);
+// Here we can't access age variable nor the printAge function here because the scope chain is a one way street: console.log(age); | printAge();
 
-const firstName = 'Jonas';
-calcAge(1991);
-// console.log(age);
-// printAge();
 
 
-///////////////////////////////////////
-// Hoisting and TDZ in Practice
+*/
+/////////////////////////////////////////////////////////////////////////// Hoisting and TDZ in Practice
 
 // Variables
 console.log(me);
@@ -92,10 +102,11 @@ const z = 3;
 console.log(x === window.x);
 console.log(y === window.y);
 console.log(z === window.z);
-*/
 
 
-///////////////////////////////////////
+
+/*
+/////////////////////////////////////////////////////////////////////////
 // The this Keyword in Practice
 console.log(this);
 
@@ -130,3 +141,4 @@ matilda.calcAge();
 const f = jonas.calcAge;
 f();
 
+*/
